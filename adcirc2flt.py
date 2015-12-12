7d4e5d7fcb68a49584c0127d01f3cfff935060f6
Pat Prodanovic <pprodano@localhost.localdomain>
2015-12-11 21:20:02 -0500

    updates

--- adcirc2flt.py
+++ adcirc2flt.py
@@ -1,7 +1,7 @@
 #
 #+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!
 #                                                                       #
-#                                 adcirc2asc.py                         # 
+#                                 adcirc2flt.py                         # 
 #                                                                       #
 #+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!
 #
@@ -9,18 +9,19 @@
 # 
 # Date: June 29, 2015
 #
-# Purpose: Script takes in a tin in ADCIRC format, and generates an ESRI *.asc 
-# file for easy visualization by a GIS.
+# Purpose: Script takes in a tin in ADCIRC format, and generates an ESRI *.flt 
+# file for easy visualization by a GIS. It works exactly as my adcirc2asc.py
+# script, except that it produces binary files instead of ascii files.
 #
 # Uses: Python2.7.9, Matplotlib v1.4.2, Numpy v1.8.2
 #
 # Example:
 #
-# python adcirc2asc.py -i tin.14 -s 10 -o tin.asc
+# python adcirc2flt.py -i tin.14 -s 10 -o tin.asc
 # where:
 # -i input adcirc mesh file
-# -s spacing (in m) of the *.asc grid file
-# -o generated *.asc grid file
+# -s spacing (in m) of the *.flt grid file
+# -o generated *.flt grid file
 # 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Global Imports
@@ -30,7 +31,8 @@ import matplotlib.tri    as mtri           # matplotlib triangulations
 import numpy             as np             # numpy
 import math                                # for the ceil function
 from ppmodules.readMesh import *           # to get all readMesh functions
-import struct
+import struct                              # to write binary data to file
+from utils.progressbar import ProgressBar  # progress bar
 # 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # MAIN
@@ -41,7 +43,7 @@ curdir = os.getcwd()
 if len(sys.argv) != 7 :
 	print 'Wrong number of Arguments, stopping now...'
 	print 'Usage:'
-	print 'python adcirc2asc.py -i tin.14 -s 10 -o tin.asc'
+	print 'python adcirc2flt.py -i tin.14 -s 10 -o tin.flt'
 	sys.exit()
 dummy1 =  sys.argv[1]
 adcirc_file = sys.argv[2]
@@ -49,10 +51,14 @@ dummy2 =  sys.argv[3]
 spacing = sys.argv[4]
 spacing = float(spacing)
 dummy3 =  sys.argv[5]
-output_file = sys.argv[6] # output *.asc grid
+output_file = sys.argv[6] # output *.flt grid
 
-# to create the output file
-fout = open(output_file,"w")
+# to create the output *.flt file
+fout = open(output_file,"wb")
+
+# the name of the header file (this is asci format)
+header_file = output_file.split('.',1)[0] + '.hdr'
+fhdr = open(header_file,"w")
 
 # read the adcirc file
 n,e,x,y,z,ikle = readAdcirc(adcirc_file)
@@ -80,32 +86,37 @@ x_regs = xreg[1,:]
 y_regs = yreg[:,1]
 
 # perform the triangulation
+print "Interpolating ..."
 interpolator = mtri.LinearTriInterpolator(tin, z)
 interp_zz = interpolator(xreg, yreg)
 
-print "Shape of array z: " + str(interp_zz.shape[0])
+#print "Shape of array z: " + str(interp_zz.shape[0])
 
-print "Shape of arrays xreg and yreg: " + str(x_regs.shape) + " " + str(y_regs.shape) 
+#print "Shape of arrays xreg and yreg: " + str(x_regs.shape) + " " + str(y_regs.shape) 
 
 where_are_NaNs = np.isnan(interp_zz)
 interp_zz[where_are_NaNs] = -999.0
 
 #np.savetxt("temp.out", z, fmt='%.2f', delimiter='') # this has no max spaces
-np.savetxt('temp.out', np.flipud(interp_zz), fmt='%10.3f', delimiter='') # this has 10 char spaces, 2 after decimal
+#np.savetxt('temp.out', np.flipud(interp_zz), fmt='%10.3f', delimiter='') # this has 10 char spaces, 2 after decimal
 
-# open the output *.asc file, and write the header info
-fout.write("NCOLS " + str(interp_zz.shape[1]) + "\n")
-fout.write("NROWS " + str(interp_zz.shape[0]) + "\n")
-fout.write("XLLCORNER " + str(x_regs[0]) + "\n")
-fout.write("YLLCORNER " + str(y_regs[0]) + "\n")
-fout.write("CELLSIZE " + str(spacing) + "\n")
-fout.write("NODATA_VALUE " + str(-999.00) + "\n")
+# open the output *.hdr file, and write the header info
+fhdr.write("NCOLS " + str(interp_zz.shape[1]) + "\n")
+fhdr.write("NROWS " + str(interp_zz.shape[0]) + "\n")
+fhdr.write("XLLCORNER " + str(x_regs[0]) + "\n")
+fhdr.write("YLLCORNER " + str(y_regs[0]) + "\n")
+fhdr.write("CELLSIZE " + str(spacing) + "\n")
+fhdr.write("NODATA_VALUE " + str(-999.00) + "\n")
+fhdr.write("BYTEORDER LSBFIRST " + "\n")
 
-# read the file, and write every line in fout
-with open("temp.out") as fp:
-	for line in fp:
-		fout.write(line);
-# remove the temp file
-print "Removing temporary files ..."
-os.remove("temp.out")
+print "Writing binary data file ..."
+pbar = ProgressBar(maxval=interp_zz.shape[0]).start()
+for i in range(interp_zz.shape[0]):
+	s = struct.pack('f'*interp_zz.shape[1], *np.flipud(interp_zz)[i,:])
+	fout.write(s)
+	pbar.update(i+1)
+fout.close()
+pbar.finish()
+
 print "All done!"
+
--- wip/xyz2asc.py
+++ wip/xyz2asc.py
@@ -1,6 +1,7 @@
 from scipy.interpolate import griddata
 import numpy as np
 import matplotlib.pyplot as plt
+import struct
 
 # this is the original xyz data
 x=np.linspace(1.,10.,20)
@@ -13,7 +14,24 @@ yi=np.linspace(1.,10.,10)
 X,Y= np.meshgrid(xi,yi)
 
 # interpolate the z
-Z = griddata((x, y), z, (X, Y),method='linear', fill_value=-999.0)
+# based on the above, Z is a 10x10 numpy array
+Z = griddata((x, y), z, (X, Y),method='nearest', fill_value=-999.0)
+
+# Z write to a binary file
+f = open('file.flt', 'wb')
+for i in range(len(xi)):
+	#for j in range(len(yi)):
+	s = struct.pack('f'*len(yi), *Z[i,:])
+	f.write(s)
+f.close()
+
+'''
+for i in range(len(xi)):
+	for j in range(len(yi)):
+		s = struct.pack('f', Z[i,j])
+		f.write(s)
+f.close()
+'''
 
 plt.contourf(X,Y,Z)
 plt.show()
