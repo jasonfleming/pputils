cb659630c29a09b385c426a0d89da861f2e6f950
Pat Prodanovic <pprodano@localhost.localdomain>
2015-12-20 17:43:28 -0500

    adcirc2slf via subprocess

--- wip/adcirc2slf.py
+++ wip/adcirc2slf.py
@@ -7,18 +7,16 @@
 #
 # Author: Pat Prodanovic, Ph.D., P.Eng.
 # 
-# Date: June 29, 2015
+# Date: December 20, 2015
 #
 # Purpose: Script takes in a mesh in ADCIRC format, and converts it to
-# TELEMAC's SELAFIN format. Does not fill IPOB array properly; it simply
-# writes boundary nodes as the IPOB array, with no knowledge of CW or CCW.
-# To be fixed in subsequent version!
+# TELEMAC's SELAFIN format.
 #
-# Uses: Python2.7.9, Matplotlib v1.4.2, Numpy v1.8.2
+# Uses: Python2.7.9, Numpy v1.8.2
 #
 # Example:
 #
-# python adcirc2sel.py -i mesh.14 -o mesh.slf
+# python adcirc2sel.py -i mesh.grd -o mesh.slf
 # where:
 # -i input adcirc mesh file
 # -o converted *.slf mesh file
@@ -27,105 +25,12 @@
 # Global Imports
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 import os,sys                              # system parameters
-from os import path
-import matplotlib.tri    as mtri           # matplotlib triangulations
-import numpy             as np             # numpy
-import math                                # for the ceil function
-# Importing pytel tools for SELAFIN parser
-pytel = os.getcwd()
-#pytel = '/home/user/opentelemac/v7p0r1/scripts/python27/'
-sys.path.append(path.join(path.dirname(sys.argv[0]),pytel))
-from parsers.parserSELAFIN import SELAFIN  
+import numpy as np                         # numpy
+import struct                              # to determine sys architecture
+import subprocess                          # to execute binaries
+from ppmodules.selafin_io import *         # SELAFIN io
+from ppmodules.readMesh import *           # for the readAdcirc function
 # 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Functions
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-def read_adcirc(adcirc_file):
-	#{{{
-	# define lists
-	x = list()
-	y = list()
-	z = list()
-	e1 = list()
-	e2 = list()
-	e3 = list()
-	
-	# each line in the file is a list object
-	line = list()
-	with open(adcirc_file, "r") as f1:
-		for i in f1:
-			line.append(i)
-		
-	# to write to a temp file nodes and 2d elements only
-	#temp_nodes_file ="temp_nodes.csv"
-	#temp_elements_file ="temp_elements.csv"
-	
-	#fout_nodes = open(temp_nodes_file,"w")
-	#fout_elements = open(temp_elements_file,"w")
-	
-	# read how elements and nodes from second line of adcirc file (line(2)
-	ele_nod_str = line[1]
-	ele_nod_list = ele_nod_str.split() 
-	e = ele_nod_list[0]
-	e = int(e)
-	n = ele_nod_list[1]
-	n = int(n)
-	
-	node_str = str("")
-	# write the temp nodes file
-	for i in range(2,n+2):
-		#fout_nodes.write(line[i])
-		node_str = line[i]
-		node_str_list = node_str.split()
-		x.append(node_str_list[1])
-		y.append(node_str_list[2])
-		z.append(node_str_list[3])
-		
-	
-	# write the temp elements file
-	for i in range(n+2,n+2+e):
-		#fout_elements.write(line[i])
-		ele_str = line[i]
-		ele_str_list = ele_str.split()
-		e1.append(ele_str_list[2])
-		e2.append(ele_str_list[3])
-		e3.append(ele_str_list[4])
-		
-	# turn these into numpy arrays
-	xx = np.zeros(n)
-	yy = np.zeros(n)
-	zz = np.zeros(n)
-	e11 = np.zeros(e)
-	e22 = np.zeros(e)
-	e33 = np.zeros(e)
-	
-	for i in range(0,n):
-		xx[i] = x[i]
-		yy[i] = y[i]
-		zz[i] = z[i]
-		
-	# -1 to change index of elements; min node number now starts at zero
-	
-	# matplotlib triangulation requires node numbering to start at zero,
-	# whereas my adcirc node numbering start at 1
-	for i in range(0,e):
-		e11[i] = int(e1[i])-1
-		e22[i] = int(e2[i])-1
-		e33[i] = int(e3[i])-1
-		
-	# stack the elements
-	ikle = np.column_stack((e11,e22,e33))  
-	
-	ikle = ikle.astype(np.int16)
-	
-	#print ikle.shape
-	#print ikle.dtype
-	
-	#print xx.shape
-	#print xx.dtype
-	#}}}
-	return n,e,xx,yy,zz,ikle
-#
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # MAIN
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~	
@@ -135,13 +40,103 @@ curdir = os.getcwd()
 if len(sys.argv) != 5 :
 	print 'Wrong number of Arguments, stopping now...'
 	print 'Usage:'
-	print 'python adcirc2sel.py -i mesh.14 -o mesh.slf'
+	print 'python adcirc2sel.py -i mesh.grd -o mesh.slf'
 	sys.exit()
 dummy1 =  sys.argv[1]
 adcirc_file = sys.argv[2]
 dummy2 =  sys.argv[3]
 output_file = sys.argv[4]
 
+########################################################################
+# this part of the code uses python's subprocess to call external,
+# compiled version of the program bnd_extr_pp.f
+# 
+# if subprocess fails, run the bnd_extr_pp.f externally, and obtain
+# gredit.bnd file for use in this script!
+########################################################################
+# to determine if the system is 32 or 64 bit
+archtype = struct.calcsize("P") * 8
+
+# to determine the IPOBO array, call pre-compiled binary bnd_extr_pp
+if (os.name == 'posix'):
+	# to move the adcirc file to ./boundary/bin
+	callstr = 'mv ' + str(adcirc_file) + ' ./boundary/bin'
+	subprocess.call(callstr, shell=True)
+
+	# change directory to get to executable
+	os.chdir('./boundary/bin')
+	
+	if (archtype == 32):
+		# make sure the binary is allowed to be executed
+		subprocess.call('chmod +x bnd_extr_pp_32',shell=True)
+		
+		# execute the binary to generate the renumbered nodes and elements
+		subprocess.call(['./bnd_extr_pp_32', adcirc_file])
+	if (archtype == 64):
+		# make sure the binary is allowed to be executed
+		subprocess.call('chmod +x bnd_extr_pp_64',shell=True)
+		
+		# execute the binary to generate the renumbered nodes and elements
+		subprocess.call(['./bnd_extr_pp_64', adcirc_file])
+
+# move the files back
+subprocess.call('mv *.bnd ' + curdir, shell=True)
+subprocess.call('mv ' + adcirc_file + ' ' + curdir, shell=True)
+	
+# change directory back
+os.chdir(curdir)
+########################################################################
+
+# if we are here, this means gredit.bnd file is generated and moved to 
+# root dir of pputils
+if (os.path.isfile('gredit.bnd') == False):
+	print 'fortran compiled program bnd_exr_pp.f was not executed!'
+	print 'Exiting ...'
+	sys.exit()
+
+# now open gredit.bnd and read the boundary data
+# read the contents of the gredit.bnd file into a master list, where each
+# item in the list is a line
+master = list()
+with open('gredit.bnd','r') as f:
+	for i in f:
+		master.append(i)
+f.close()
+
+# this is the number of boundaries read from the gredit.bnd file
+# first line is ADCIRC, second line is num_bnd 
+num_bnd = int(master[1].split()[0])
+
+# delete the first two records in the master
+del master[0]
+del master[0]
+
+# create a list for each boundary
+# bnd is a list of lists
+bnd = list()
+for i in range(num_bnd):
+	bnd.append(list())
+
+# total boundary nodes for each boundary
+bnd_nodes = list()
+
+# initialize count
+count = -1
+
+# get how many arrays are in each boundary
+for i in range(len(master)):
+	tmp = master[i].split()
+	if (len(tmp) == 2):
+		bnd_nodes.append(tmp[0])
+		count = count + 1
+	else:
+		bnd[count].append(tmp[0])
+
+# bnd[0] is the main land boundary
+# 
+
+
+'''
 # read the adcirc file
 n,e,x,y,z,ikle = read_adcirc(adcirc_file)
 
@@ -199,4 +194,4 @@ slf2d.fole.update({ 'float': ('f',4) })  # single precision
 slf2d.appendHeaderSLF()
 slf2d.appendCoreTimeSLF(0) 
 slf2d.appendCoreVarsSLF([z])
-
+'''
