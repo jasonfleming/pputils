0e5075b4e58c22bf692aaafc9318339c5f35dffc
Pat Prodanovic <pprodano@gmail.com>
2016-11-06 22:38:33 -0500

    updates

---
+++ interpBreakline_from_pts.py
@@ -0,0 +1,130 @@
+#
+#+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!
+#                                                                       #
+#                                 interpBreakline_from_pts.py           # 
+#                                                                       #
+#+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!
+#
+# Author: Pat Prodanovic, Ph.D., P.Eng.
+# 
+# Date: Nov 6, 2016
+#
+# Purpose: Script takes in a xyz points file and a breakline file (in 
+# pputils format),  and interpolates the nodes of the breakline file 
+# from the points file. It uses scipy's kdtree to assign to breakline
+# nodes the point in the xyz dataset that is closest. It is similar to
+# the interpMesh_from_pts.py, except rather than a mesh, the input is a
+# breakline file. Used primarily to assign nodes to a TIN crop boundary.
+# If wanting to interpolate breaklines from a TIN, use interpBreakline.py
+# script. 
+#
+# This script does exactly the same thing as the mkbreakline.py script
+# with the interpolation flag on, but does it in a slightly different way.
+#
+# Uses: Python 2 or 3, Matplotlib, Numpy
+#
+# Example:
+#
+# python interpBreakline_from_pts.py -p points.csv -l lines.csv -o lines_3d.csv -n 10
+# where:
+# -p xyz points file, no headers, comma delimited
+# -l lines file (to be interpolated)
+# -o interpolated lines file
+# -n number of nearest neighbours
+# 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Global Imports
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+import os,sys                              # system parameters
+import numpy             as np             # numpy
+from scipy import spatial                  # scipy to get kdTree
+from ppmodules.readMesh import *           # to get all readMesh functions
+from progressbar import ProgressBar, Bar, Percentage, ETA
+# 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# MAIN
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~	
+curdir = os.getcwd()
+#
+# I/O
+if len(sys.argv) != 9 :
+	print('Wrong number of Arguments, stopping now...')
+	print('Usage:')
+	print('python interpBreakline_from_pts.py -p points.csv -l lines.csv -o lines_3d.csv -n 10')
+	sys.exit()
+
+pts_file = sys.argv[2]
+lines_file = sys.argv[4]
+output_file = sys.argv[6]
+neigh = int(sys.argv[8])
+
+# I am imposing a limit on neigh to be between 1 and 10
+if ((neigh < 1) or (neigh > 10)):
+	print('Number of neighbours must be between 1 and 10. Exiting.')
+	sys.exit(0)
+
+print('Reading input data')
+# read the points file
+pts_data = np.loadtxt(pts_file, delimiter=',',skiprows=0,unpack=True)
+x = pts_data[0,:]
+y = pts_data[1,:]
+z = pts_data[2,:]
+
+# read the lines file
+lines_data = np.loadtxt(lines_file, delimiter=',',skiprows=0,unpack=True)
+shapeid = lines_data[0,:]
+#shapeid = shapeid.astype(np.int32)
+lns_x = lines_data[1,:]
+lns_y = lines_data[2,:]
+lns_z = np.zeros(len(lns_x))
+
+print('Constructing KDTree object')
+# to create a KDTree object our of the xyz points file
+source = np.column_stack((x,y))
+tree = spatial.KDTree(source)
+
+den = 0.0
+tmp_sum = 0.0
+
+print('Interpolating')
+w = [Percentage(), Bar(), ETA()]
+pbar = ProgressBar(widgets=w, maxval=len(lns_x)).start()
+for i in range(len(lns_x)):
+	d,idx = tree.query((lns_x[i],lns_y[i]), k = neigh)
+	
+	# calculate the denominator
+	if neigh > 1:
+		for j in range(neigh):
+			den = den + (1.0 / (d[j]**2))
+	else:
+		den = den + (1.0 / (d**2))
+		
+	# calculate the weights
+	weights = (1.0 / d**2) / den
+	
+	# to assign the interpolated value
+	if neigh > 1:
+		for j in range(neigh):
+			tmp_sum = tmp_sum + weights[j]*z[idx[j]]
+	else:
+		tmp_sum = weights * z[idx]
+		
+	# now assign the value	
+	lns_z[i] = tmp_sum
+	
+	# reset the denominator
+	den = 0.0
+	tmp_sum = 0.0
+	
+	pbar.update(i+1)
+pbar.finish()
+
+print('Writing results to file')
+# to create the output file (this is the interpolated mesh)
+fout = open(output_file,'w')
+
+# now to write the interpolated lines file (and its node values)
+for i in range(len(lns_x)):
+	fout.write(str(shapeid[i]) + "," + str(lns_x[i]) + "," + \
+		str(lns_y[i]) + "," +  str(lns_z[i]) + "\n")
+
---
+++ interpMesh.py
@@ -0,0 +1,114 @@
+#
+#+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!
+#                                                                       #
+#                                 interpMesh.py                         # 
+#                                                                       #
+#+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!
+#
+# Author: Pat Prodanovic, Ph.D., P.Eng.
+# 
+# Date: June 28, 2015
+#
+# Modified: Feb 21, 2016
+# Made it work under python 2 or 3
+#
+# Modified: Nov 6, 2016
+# Changed the name of the script from interp.py to interpMesh.py
+#
+# Purpose: Script takes in a tin and a mesh file (both in ADCIRC format), 
+# and interpolates the nodes of the mesh file from the tin.
+#
+# Uses: Python 2 or 3, Matplotlib, Numpy
+#
+# Example:
+#
+# python interpMesh.py -t tin.grd -m mesh.grd -o mesh_interp.grd
+# where:
+# -t tin surface
+# -m mesh (whose nodes are to be interpolated)
+# -o interpolated mesh
+# 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Global Imports
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+import os,sys                              # system parameters
+import matplotlib.tri    as mtri           # matplotlib triangulations
+import numpy             as np             # numpy
+from ppmodules.readMesh import *           # to get all readMesh functions
+# 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# MAIN
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~	
+curdir = os.getcwd()
+#
+# I/O
+if len(sys.argv) != 7 :
+	print('Wrong number of Arguments, stopping now...')
+	print('Usage:')
+	print('python interpMesh.py -t tin.grd -m mesh.grd -o mesh_interp.grd')
+	sys.exit()
+
+dummy1 =  sys.argv[1]
+tin_file = sys.argv[2]
+dummy2 =  sys.argv[3]
+mesh_file = sys.argv[4]
+dummy3 =  sys.argv[5]
+output_file = sys.argv[6] # interp_mesh
+
+# read the adcirc tin file
+t_n,t_e,t_x,t_y,t_z,t_ikle = readAdcirc(tin_file)
+
+# read the adcirc mesh file
+# this one has z values that are all zeros
+m_n,m_e,m_x,m_y,m_z,m_ikle = readAdcirc(mesh_file)
+
+# create tin triangulation object using matplotlib
+tin = mtri.Triangulation(t_x, t_y, t_ikle)
+
+# to perform the triangulation
+interpolator = mtri.LinearTriInterpolator(tin, t_z)
+m_z = interpolator(m_x, m_y)
+
+# if the node is outside of the boundary of the domain, assign value -999.0
+# as the interpolated node
+where_are_NaNs = np.isnan(m_z)
+m_z[where_are_NaNs] = -999.0
+'''
+if (np.sum(where_are_NaNs) > 0):
+	print '#####################################################'
+	print ''
+	print 'WARNING: Some nodes are outside of the TIN boundary!!!'
+	print ''
+	print 'Closest TIN node is assigned to those nodes!'
+	print ''
+	print '#####################################################'
+'''
+
+# rather than keeping the -999.0 as the mesh node value outside the tin,
+# simply assign to that mesh node the elevation of the closest tin node.
+for i in range(len(m_x)):
+	if (where_are_NaNs[i] == True):
+		xdist = np.subtract(t_x,m_x[i])
+		ydist = np.subtract(t_y,m_y[i])
+		dist = np.sqrt(np.power(xdist,2.0) + np.power(ydist,2.0))
+		minidx = np.argmin(dist)
+		
+		m_z[i] = t_z[minidx]
+
+# to create the output file
+fout = open(output_file,"w")
+
+# now to write the adcirc mesh file
+fout.write("ADCIRC" + "\n")
+# writes the number of elements and number of nodes in the header file
+fout.write(str(m_e) + " " + str(m_n) + "\n")
+
+# writes the nodes
+for i in range(0,m_n):
+	fout.write(str(i+1) + " " + str("{:.3f}".format(m_x[i])) + " " + 
+		str("{:.3f}".format(m_y[i])) + " " + str("{:.3f}".format(m_z[i])) + "\n")
+#
+# writes the elements
+for i in range(0,m_e):
+	fout.write(str(i+1) + " 3 " + str(m_ikle[i,0]+1) + " " + str(m_ikle[i,1]+1) + " " + 
+		str(m_ikle[i,2]+1) + "\n")
