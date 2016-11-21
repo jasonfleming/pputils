9c65c54c34a824bafeb61c5c9f8470f8547de737
Pat Prodanovic <pprodano@gmail.com>
2016-11-20 21:45:17 -0500

    updates

--- examples/invalid_tin/test_invalid_TIN.py
+++ examples/invalid_tin/test_invalid_TIN.py
@@ -1,6 +1,7 @@
 #import ppmodules.readMesh as pp
 import matplotlib.tri as mtri
 import numpy as np
+import matplotlib.pyplot as plt
 
 # reads an existing triangulation from a file
 #n,e,x,y,z,ikle = pp.readAdcirc('zero_area_element.grd')
@@ -10,11 +11,17 @@ x = np.array([0.0, 1.0, 1.0, 1.0, 2.0])
 y = np.array([1.0, 0.0, 2.0, 1.0, 1.0])
 z = np.zeros(5)
 
-triangles = np.array( [[0, 1, 2], [1, 3, 2], [1, 4, 2], [0, 4, 1]])
+# slightly modified from what I originally posted on Matplotlib's mailing list
+triangles = np.array( [[0, 1, 4], [2, 3, 4], [0, 3, 2], [0, 3, 4]])
 
 # create a Matplotlib Triangulation
 triang = mtri.Triangulation(x,y,triangles)
 
 # to perform the linear interpolation
-interpolator = mtri.LinearTriInterpolator(triang, z)
-m_z = interpolator(1.0, 1.0)
+#interpolator = mtri.LinearTriInterpolator(triang, z)
+#m_z = interpolator(1.0, 1.0)
+
+plt.figure()
+plt.gca().set_aspect('equal')
+plt.triplot(triang, color='black')
+plt.show()
