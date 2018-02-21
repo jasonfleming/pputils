30fb2d08da7748631d2f793b7f848ee752d29f36
Pat Prodanovic <pprodano@gmail.com>
2018-02-21 11:12:59 +0100

    updates

---
+++ spe2sel.py
@@ -0,0 +1,111 @@
+#
+#+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!
+#                                                                       #
+#                                 spe2sel.py                            # 
+#                                                                       #
+#+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!
+#
+# Author: Pat Prodanovic, Ph.D., P.Eng.
+# 
+# Date: Feb 21, 2018
+#
+# Purpose: Script takes in a spectral file produced by TOMAWAC, and
+# converts it to a *.slf file. Essentially, the quad mesh used by
+# the TOMAWAC's spectral file is converted to a triangular mesh,
+# which could then be used by any post-processor that reads *.slf files.
+#
+# Uses: Python 2 or 3, Numpy
+#
+# Example:
+#
+# python spe2png.py -i out.spe -o out.slf
+# where:
+# -i input spectral file (ikle are quads, NDP = 4 in this case)
+# -o output selafin file (ikle are triangles, NDP = 3(
+#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Global Imports
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+import os,sys
+import numpy as np
+from ppmodules.selafin_io_pp import *
+# 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# MAIN
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
+curdir = os.getcwd()
+
+if len(sys.argv) == 5:
+  input_file = sys.argv[2]
+  output_file = sys.argv[4]
+else:
+  print('Wrong number of Arguments, stopping now...')
+  print('Usage:')
+  print('python spe2sel.py -i input.spe -o output.slf')
+  sys.exit()  
+
+# read the existing spectral file
+spe = ppSELAFIN(input_file)
+spe.readHeader()
+spe.readTimes()
+
+# gets the precision of the file
+float_type, float_size = spe.getPrecision()
+
+# gets the variable names and units
+vnames = spe.getVarNames()
+vunits = spe.getVarUnits()
+
+# the number of variables
+numvars = len(vnames)
+
+# index of the time step
+times = spe.getTimes()
+
+print(times)
+
+# gets the info from the spectral file and stores into approrpiate vars
+ELEM,NPOIN,NDP,IKLE,IPOBO,x,y = spe.getMesh()
+
+# to check if the input file is really a spectral file
+if (NDP !=4):
+  print('The input file has triangle, not quads. Exiting!.')
+  sys.exit(0)
+
+# now we split the quads into triangles
+ELEM2 = ELEM * 2
+IKLE2 = np.zeros( (ELEM2, 3), dtype=np.int32)
+
+for i in range(ELEM):
+  IKLE2[2*i-1,0] = IKLE[i,0]
+  IKLE2[2*i-1,1] = IKLE[i,1]
+  IKLE2[2*i-1,2] = IKLE[i,2]
+  
+  IKLE2[2*i,0] = IKLE[i,0]
+  IKLE2[2*i,1] = IKLE[i,2]
+  IKLE2[2*i,2] = IKLE[i,3]
+
+# re-set the NDP variable to 3 (triangles)
+NDP = 3
+
+# now we can write the front matter of the output file
+slf = ppSELAFIN(output_file)
+slf.setPrecision(float_type,float_size)
+slf.setTitle('created with pputils')
+slf.setVarNames(vnames)
+slf.setVarUnits(vunits)
+slf.setIPARAM([1, 0, 0, 0, 0, 0, 0, 0, 0, 1])
+slf.setMesh(ELEM2, NPOIN, NDP, IKLE2, IPOBO, x, y)
+slf.writeHeader()
+
+# now go through all the time steps and copy data
+# to the *.slf container above
+for t in range(len(times)):
+  res = np.zeros( (numvars, NPOIN) )
+
+  # reads the data from the spectral file
+  spe.readVariables(t)
+  res = spe.getVarValues()
+
+  # writes the data to the selafin file
+  slf.writeVariables(times[t], res)
--- transp.py
+++ transp.py
@@ -66,48 +66,48 @@ from progressbar import ProgressBar, Bar, Percentage, ETA
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # converts cartesian vector to Tomawac nautical direction convention
 def toTomNautical(u,v):
-	
-	# quadrants are defined as follows
-	# IV  | I
-	# ---------
-	# III | II
-	
-	# error checking
-	if (abs(v) < 1.0E-6):
-		v = 1.0E-6
-	if (abs(u) < 1.0E-6):
-		u = 1.0E-6
-		
-	# compute the cartesian angle
-	theta_cart = np.arctan(abs(v)/abs(u)) * 360.0 / (2.0 * np.pi)
-	
-	# quadrant I
-	if (u >= 0.0 and v >= 0.0):
-		theta_naut = 90 - theta_cart 
-	# quadrant II
-	elif (u > 0.0 and v < 0.0):
-		theta_naut = 90 + theta_cart
-	# quadrant III
-	elif (u < 0.0 and v < 0.0):
-		theta_naut = 270 - theta_cart
-	# quadrant IV
-	elif (u < 0.0 and v > 0.0):
-		theta_naut = 270 + theta_cart
-	else:
-		theta_naut = 0.0
-	
-	dir = theta_naut
-	
-	return dir
+  
+  # quadrants are defined as follows
+  # IV  | I
+  # ---------
+  # III | II
+  
+  # error checking
+  if (abs(v) < 1.0E-6):
+    v = 1.0E-6
+  if (abs(u) < 1.0E-6):
+    u = 1.0E-6
+    
+  # compute the cartesian angle
+  theta_cart = np.arctan(abs(v)/abs(u)) * 360.0 / (2.0 * np.pi)
+  
+  # quadrant I
+  if (u >= 0.0 and v >= 0.0):
+    theta_naut = 90 - theta_cart 
+  # quadrant II
+  elif (u > 0.0 and v < 0.0):
+    theta_naut = 90 + theta_cart
+  # quadrant III
+  elif (u < 0.0 and v < 0.0):
+    theta_naut = 270 - theta_cart
+  # quadrant IV
+  elif (u < 0.0 and v > 0.0):
+    theta_naut = 270 + theta_cart
+  else:
+    theta_naut = 0.0
+  
+  dir = theta_naut
+  
+  return dir
 
 curdir = os.getcwd()
 #
 # I/O
 if len(sys.argv) != 7 :
-	print('Wrong number of Arguments, stopping now...')
-	print('Usage:')
-	print('python transp.py -r result.slf -m mesh.slf -o mesh_transp.slf')
-	sys.exit()
+  print('Wrong number of Arguments, stopping now...')
+  print('Usage:')
+  print('python transp.py -r result.slf -m mesh.slf -o mesh_transp.slf')
+  sys.exit()
 
 result_file = sys.argv[2]
 mesh_file = sys.argv[4]
@@ -135,9 +135,9 @@ numvars = len(vnames)
 # and that the direction variable is in Tomawac's naut convention
 dir_idx = -999
 for i in range(numvars):
-	if ((vnames[i].find('DIRECTION') > -1)):
-		dir_idx = i
-		
+  if ((vnames[i].find('DIRECTION') > -1)):
+    dir_idx = i
+    
 # subscript r is for the result file
 NELEM_r, NPOIN_r, NDP_r, IKLE_r, IPOBO_r, x_r, y_r = res.getMesh()
 
@@ -177,56 +177,56 @@ pbar = ProgressBar(widgets=w, maxval=len(times)).start()
 # to perform triangulation for each variable in the result file, 
 # for each time step
 for t in range(len(times)):
-	pbar.update(t+1)
-	# print('Writing time step: ' + str(t))
-	# this is the master transposed array, for time step t
-	mesh_results = np.zeros((numvars, NPOIN_m))
-	
-	# reads the results for a particular variable, and stores it into results
-	res.readVariables(t)
-	results = res.getVarValues()
-	
-	# perform the interpolation and create mesh_results array
-	for i in range(numvars):
-		
-		# correction for direction variable
-		# +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
-		if (i == dir_idx):
-			wavex = np.sin(results[i,:]*np.pi/180.0)
-			wavey = np.cos(results[i,:]*np.pi/180.0)
-			
-			# interpolate the direction variable
-			interpolator = mtri.LinearTriInterpolator(triang, wavex)
-			wavex_int = interpolator(x_m, y_m)
-			
-			interpolator = mtri.LinearTriInterpolator(triang, wavey)
-			wavey_int = interpolator(x_m, y_m)
-			
-			for j in range(NPOIN_m):
-				if (np.isnan(wavex_int[j])):
-					# rather than assigning zero, assign a value from a closest non-nan node
-					d, idx = tree.query((x_m[j],y_m[j]), k = 1)
-					wavex_int[j] = wavex[idx]
-					
-				if (np.isnan(wavey_int[j])):
-					# rather than assigning zero, assign a value from a closest non-nan node
-					d, idx = tree.query((x_m[j],y_m[j]), k = 1)
-					wavey_int[j] = wavey[idx]
-					
-				# from wavex_int and wavey_int, re-create the direction variable
-				# direction is in tomawac's nautical convention
-				mesh_results[i,j] = toTomNautical(wavex_int[j],wavey_int[j])
-		# +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
-		else:
-			# this is for the rest of the variables
-			interpolator = mtri.LinearTriInterpolator(triang, results[i,:])
-			mesh_results[i,:] = interpolator(x_m, y_m)
-		
-			for j in range(NPOIN_m):
-				if (np.isnan(mesh_results[i,j])):
-					# rather than assigning zero, assign a value from a closest non-nan node
-					d, idx = tree.query((x_m[j],y_m[j]), k = 1)
-					mesh_results[i,j] = results[i][idx]
+  pbar.update(t+1)
+  # print('Writing time step: ' + str(t))
+  # this is the master transposed array, for time step t
+  mesh_results = np.zeros((numvars, NPOIN_m))
+  
+  # reads the results for a particular variable, and stores it into results
+  res.readVariables(t)
+  results = res.getVarValues()
+  
+  # perform the interpolation and create mesh_results array
+  for i in range(numvars):
+    
+    # correction for direction variable
+    # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+    if (i == dir_idx):
+      wavex = np.sin(results[i,:]*np.pi/180.0)
+      wavey = np.cos(results[i,:]*np.pi/180.0)
+      
+      # interpolate the direction variable
+      interpolator = mtri.LinearTriInterpolator(triang, wavex)
+      wavex_int = interpolator(x_m, y_m)
+      
+      interpolator = mtri.LinearTriInterpolator(triang, wavey)
+      wavey_int = interpolator(x_m, y_m)
+      
+      for j in range(NPOIN_m):
+        if (np.isnan(wavex_int[j])):
+          # rather than assigning zero, assign a value from a closest non-nan node
+          d, idx = tree.query((x_m[j],y_m[j]), k = 1)
+          wavex_int[j] = wavex[idx]
+          
+        if (np.isnan(wavey_int[j])):
+          # rather than assigning zero, assign a value from a closest non-nan node
+          d, idx = tree.query((x_m[j],y_m[j]), k = 1)
+          wavey_int[j] = wavey[idx]
+          
+        # from wavex_int and wavey_int, re-create the direction variable
+        # direction is in tomawac's nautical convention
+        mesh_results[i,j] = toTomNautical(wavex_int[j],wavey_int[j])
+    # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+    else:
+      # this is for the rest of the variables
+      interpolator = mtri.LinearTriInterpolator(triang, results[i,:])
+      mesh_results[i,:] = interpolator(x_m, y_m)
+    
+      for j in range(NPOIN_m):
+        if (np.isnan(mesh_results[i,j])):
+          # rather than assigning zero, assign a value from a closest non-nan node
+          d, idx = tree.query((x_m[j],y_m[j]), k = 1)
+          mesh_results[i,j] = results[i][idx]
 
-	mres.writeVariables(times[t], mesh_results)
+  mres.writeVariables(times[t], mesh_results)
 pbar.finish()
